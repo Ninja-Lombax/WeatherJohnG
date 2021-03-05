a0fb737763cbc64913a8290f58eda367ac7a2268
ninjalombax <godzillakotm80@gmail.com>
2021-03-04 21:29:19 -0600

    Initial commit

--- weatherJohnG/ViewController.swift
+++ weatherJohnG/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let url = NSURL(string: "https://api.darksky.net/forecast/ae58c5fa7285b492f6a553d200018d9e/42.248341,-88.608465")
+        {
+            if let data = NSData(contentsOf: url as URL)
+            {
+                do
+                {
+                    
+                    let parsed = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String:AnyObject]
+                    
+                    let newDict = parsed
+                    
+                    print(newDict["currently"]!["summary"] as Any)
+                    print(newDict["currently"]!["temperature"] as Any)
+                    print(newDict["currently"]!["dewpoint"] as Any)
+                    
+                    self.currentTemp.text = "\(newDict["currently"]!["temperature"]!!)"
+                    self.currentSummary.text = "\(newDict["currently"]!["summary"]!!)"
+                    self.currentDewpoint.text = "\(newDict["currently"]!["dewPoint"]!!)"
+                }
+                catch let error as NSError {
+                    print("A JSON parsing error occurred. Here are the details:\n \(error)")
+                    
+                }
+            }
+        }
     }
-
-
+        
+    @IBOutlet var currentTemp: UILabel!
+    @IBOutlet var currentSummary: UILabel!
+    
+    @IBOutlet var currentDewpoint: UILabel!
+    
+    
 }
+
+
+
 
